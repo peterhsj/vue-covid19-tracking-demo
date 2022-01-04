733e868afcd6774dae0ced6ba5ddb4da9fd5172e
PeterHuang <peterhuang@inquartik.com>
2022-01-04 13:03:34 +0800

    feat: 安裝vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
