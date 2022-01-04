c84e8d1e7227b678c7a95fbf66ef7c4262492d57
PeterHuang <peterhuang@inquartik.com>
2022-01-04 15:41:33 +0800

    feat: 調整vue.config設定

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  filenameHashing: false,
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
 }
