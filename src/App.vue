c5eeb2f0c531c743da5ded5f76c38f9d4a673600
PeterHuang <peterhuang@inquartik.com>
2022-01-04 13:04:36 +0800

    feat: 將chat page 另存一頁

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <v-app>
     <v-main>
-      <h1>COVID-19 Tracking Dashboard</h1>
-      <LineChart />
+      <Covid />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import axios from 'axios';
-import LineChart from './components/LineChart'
+import Covid from './components/covid/Covid.vue'
 
 export default {
   name: 'App',
   components: {
-    LineChart,
+    Covid,
   },
   data() {
     return {};
-  },
-  async created() {
-    let { data } = await axios.get(
-      'https://api.covidtracking.com/v1/us/daily.json'
-    )
-    console.log(data)
   },
 };
 </script>
---
+++ src/components/covid/Covid.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-main>
+      <h1>COVID-19 Tracking Dashboard</h1>
+      <LineChart />
+    </v-main>
+  </v-app>
+</template>
+<script>
+import axios from 'axios';
+import LineChart from './LineChart'
+
+export default {
+  name: 'App',
+  components: {
+    LineChart,
+  },
+  data() {
+    return {};
+  },
+  async created() {
+    let { data } = await axios.get(
+      'https://api.covidtracking.com/v1/us/daily.json'
+    )
+    console.log(data)
+  },
+  
+}
+</script>
